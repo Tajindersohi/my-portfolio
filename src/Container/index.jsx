71bf7de25d1af55aa1b1e4a879166183bf50e60f
Tajindersohi <126231353+Tajindersohi@users.noreply.github.com>
2024-05-05 12:35:25 +0530

    Update : logo

--- src/Container/index.jsx
+++ src/Container/index.jsx
@@ -4,8 +4,8 @@ import logo from '../Components/Images/logosmall.png';
 function NavBar({ handleTab }) {
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-      <img src={logo} alt="logo" height={'50px'} width={'50px'} style={{marginLeft:'10px'}}/>
-      {/* <Link className="navbar-brand" to="/"><span style={{fontSize:'20px', fontWeight:"xxx-large"}}>Tajinder</span></Link> */}
+      {/* <img src={logo} alt="logo" height={'50px'} width={'50px'} style={{marginLeft:'10px'}}/> */}
+      <Link className="navbar-brand" to="/"><span style={{fontSize:'20px', fontWeight:"xxx-large"}}>Tajinder</span></Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
