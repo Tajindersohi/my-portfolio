5d6189ba1be3bcfa0974cb22ed91190873882c94
Tajindersohi <126231353+Tajindersohi@users.noreply.github.com>
2025-07-12 12:59:04 +0530

    Updated Resume

--- src/Container/Pages/Home.jsx
+++ src/Container/Pages/Home.jsx
@@ -90,8 +90,12 @@ function Home() {
                             <Box my={5} textAlign={'left'}>
                                 <Grid container spacing={2} justifyContent="left">
                                     <Grid item xs={12} sm={12} md={4} textAlign={'center'}> {/* Change sm={6} to sm={4} */}
-                                        <a style={{ width: '80%' }} href="/Pdf/TajinderResume.pdf" className="project-button text-center" download>
-                                            Download CV
+                                        <a style={{ width: '80%' }} href="https://drive.google.com/file/d/1sHe3ll0hrCfJB3lMSB2BTQd6yCPIOlQ1/view?usp=sharing" className="project-button text-center" download>
+                                            <span className='pr-2'>Resume</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="2 0 18 18">
+                                                <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+                                                <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+                                            </svg>
                                         </a>
                                     </Grid>
                                     <Grid item xs={12} sm={12} md={4} textAlign={'center'}> {/* Change sm={6} to sm={4} */}
